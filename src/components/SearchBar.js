1ce1db70b18e9a2931ffc3e7681ec4dfaeb624a3
colbisaurusrex <c.barthelmess1@gmail.com>
2016-12-27 15:21:56 -0500

    changes HelloMessage to SearchBar and renders an input field to DOM instead of a hello message

---
+++ src/components/SearchBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+//this is a class based component
+class SearchBar extends Component{
+
+    constructor(props){
+      super(props)
+    }
+
+    render(){
+      return (
+        <input />
+      )
+    }
+};
+
+export default SearchBar;
