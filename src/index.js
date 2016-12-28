f51d5a44a8128a182ddf3641bad05d2dd6c4539d
colbisaurusrex <c.barthelmess1@gmail.com>
2016-12-28 14:00:40 -0500

    adds tacoList and tacoMap classNames

--- src/index.js
+++ src/index.js
@@ -3,11 +3,22 @@ import ReactDOM from 'react-dom';
 
 import SearchBar from './components/SearchBar.js';
 
+//google maps api key
+const API_KEY = 'AIzaSyATwL4uEOxEdZYgb6t-YHFUGsQkgTgBu_o'
+
 //this is a functional component
 const App = () => {
   return (
-    <div>
-    <SearchBar />
+   <div>
+      <div>
+      <SearchBar />
+      </div>
+      <div className="tacoMap">
+        this is where the map will go.
+      </div>
+      <div className="tacoList">
+        this is where the list of taco joints will go.
+      </div>
     </div>
     )
 }
