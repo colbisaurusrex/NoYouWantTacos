28d00f4526b0c17db9586e1db77d1c4bedc6ed23
colbisaurusrex <c.barthelmess1@gmail.com>
2016-12-28 13:58:40 -0500

    adds link to new stylesheet

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-//this is a class based component
+//this is a class based component. Used whenever we need to be aware of state.
 class SearchBar extends Component{
 
     //all js classes have a function called constructor, it's the only one called automatically
@@ -23,10 +23,14 @@ class SearchBar extends Component{
       return (
          // onChange is an event listener
          //"this" loses context, so we have to bind it
+         //this.setState triggers a re rendering in React.js
+         //the value of the input is then changed after the handleInputChange is called. In this way, it is a controlled component and it is being told what its value is, instead of the other way around.
         <div>
-          <p>Enter a city and a food item, i.e. pasta</p>
-          <input onChange={this.handleInputChange.bind(this)}/>
-          <p>This isn't tacos: {this.state.userInput}</p>
+          <p>Enter a food item, i.e. pasta</p>
+          <input
+            value={this.state.userInput}
+            onChange={this.handleInputChange.bind(this)}/>
+          <p>{this.state.userInput} isn't tacos</p>
         </div>
 
 
