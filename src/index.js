dd92e3edbddb424f44323f30fdf9ea2ef342a781
colbisaurusrex <c.barthelmess1@gmail.com>
2016-12-28 16:58:00 -0500

    links googlemap javascript library, adds distinct red and blue backgrounds to both Map and Places components

--- src/index.js
+++ src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from 'react-dom';
 
 import SearchBar from './components/SearchBar.js';
 import Map from './components/Map.js'
-import TacoList from './components/TacoList.js'
+import Places from './components/Places.js'
 
 //google maps api key
-const API_KEY = 'AIzaSyATwL4uEOxEdZYgb6t-YHFUGsQkgTgBu_o'
+// const API_KEY = 'AIzaSyATwL4uEOxEdZYgb6t-YHFUGsQkgTgBu_o'
 
 //this is a functional component
 const App = () => {
   return (
    <div>
       <SearchBar />
-      <Map />
-      <TacoList />
+      <div className="map">
+        <Map />
+      </div>
+      <div className="places">
+        <Places />
+      </div>
   </div>
     )
 }
