c4ef74df316c5979d2ffaad6f449aa408bddfa58
colbisaurusrex <c.barthelmess1@gmail.com>
2016-12-29 10:13:14 -0500

    imports react-google-maps, adds map loader, renders a google map to the page

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
+import { GoogleMapLoader, GoogleMap, Marker } from 'react-google-maps'
 
 class Map extends Component {
+
+
   render() {
+
+    const mapContainer = <div style={{height: '100%', width: '100%'}}></div>
+
     return (
-      <div>This is the Map</div>
+        <GoogleMapLoader
+            containerElement = { mapContainer }
+            googleMapElement = {
+              <GoogleMap
+                  defaultZoom = {6}
+                  defaultCenter={this.props.location}
+                  options = {{streetViewControl: false, mapTypeControl: true}}>
+                  </GoogleMap>
+            } />
       );
+
   }
 };
 
