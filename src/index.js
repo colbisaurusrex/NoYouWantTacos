375857e12d67bb18c0eec12ad460098e9e9b2653
colbisaurusrex <c.barthelmess1@gmail.com>
2016-12-29 11:32:38 -0500

    changes app to class based component, adds marker and location props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/SearchBar.js';
 import Map from './components/Map.js'
 import Places from './components/Places.js'
@@ -9,24 +8,37 @@ import Places from './components/Places.js'
 // const API_KEY = 'AIzaSyATwL4uEOxEdZYgb6t-YHFUGsQkgTgBu_o'
 
 //this is a functional component
-const App = () => {
+class App extends Component {
 
+
+
+      render() {
       const location = {
-        lat: 30,
-        lng: 80
-      }
+          lat: 28.61,
+          lng: 77.20
+        }
 
-      return (
-       <div>
-          <SearchBar />
-          <div className="map">
-            <Map location={location}/>
-          </div>
-          <div className="places">
-            <Places />
-          </div>
-      </div>
-        )
+      const pins = [
+          {
+            location: {
+              lat: 28.61,
+              lng: 77.20
+            }
+          }
+        ]
+
+        return (
+             <div>
+                <SearchBar />
+                <div className="map">
+                  <Map location={location} pins={pins}/>
+                </div>
+                <div className="places">
+                  <Places />
+                </div>
+            </div>
+            );
+        }
 }
 
 ReactDOM.render(<App />, document.getElementById("hello"));
