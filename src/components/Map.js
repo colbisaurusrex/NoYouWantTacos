d2b68c00b4afae8f78361f062ea0c83f8513c7cc
colbisaurusrex <c.barthelmess1@gmail.com>
2016-12-29 11:32:07 -0500

    adds marker component  to Map component

--- src/components/Map.js
+++ src/components/Map.js
@@ -8,14 +8,27 @@ class Map extends Component {
 
     const mapContainer = <div style={{height: '100%', width: '100%'}}></div>
 
+    const pins = this.props.pins.map((restaurant, i)=>{
+        const pin = {
+          position: {
+            lat: restaurant.location.lat,
+            lng: restaurant.location.lng
+          }
+        }
+        //... is a spread operator
+        //Marker is a component imported from react-google-maps
+        return <Marker key={i} {...pin} />
+    })
+
     return (
         <GoogleMapLoader
             containerElement = { mapContainer }
             googleMapElement = {
               <GoogleMap
-                  defaultZoom = {6}
+                  defaultZoom = {15}
                   defaultCenter={this.props.location}
                   options = {{streetViewControl: false, mapTypeControl: true}}>
+                  {pins}
                   </GoogleMap>
             } />
       );
